b3b44ded3d58f253064c7345fe9435276f5b6d35
Zill Ding <zilldzy@gmail.com>
2016-03-26 23:27:19 +0800

    Change the data and data api

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -1,40 +1,55 @@
 const fs = require('fs')
 const csv = require('fast-csv')
 
-const INPUT_FILE = process.argv[2] || `${__dirname}/data.csv`
-const OUTPUT_FILE = `${__dirname}/../lib/data.json`
+const INPUT_FILE_DIR = `${__dirname}/csv/enrolment`
+const OUTPUT_FILE_DIR = `${__dirname}/../lib/data/enrolment`
 
-const inputStream = fs.createReadStream(INPUT_FILE)
-const outputStream = fs.createWriteStream(OUTPUT_FILE)
-
-const names = ['TTSH', 'CGH', 'SGH', 'NUH(A)', 'NTFGH', 'KTPH']
-const result = []
-
-const csvStream = csv({
-  headers: true
+const files = ['hd', 'pad', 'pd', 'ufd']
+files.forEach(name => {
+  const input = `${INPUT_FILE_DIR}/${name}.csv`
+  const output = `${OUTPUT_FILE_DIR}/${name}.json`
+  convertToJson(input, output)
 })
-.on('data', function(data){
-  const waitingTime = {}
-  names.map(name => {
-    waitingTime[name] = Number(data[name])
+
+// convert a csv file to a json file
+function convertToJson(inputFilePath, outputFilePath) {
+  const inputStream = fs.createReadStream(inputFilePath)
+  const outputStream = fs.createWriteStream(outputFilePath)
+
+  const result = []
+
+  const csvStream = csv({
+    headers: true
+  })
+  .on('data', function(data){
+    result.push(process(data))
+  })
+  .on('end', function(){
+    // write the result to output file
+    outputStream.write(
+      JSON.stringify(result, null, 2),
+      err => {
+        if (err) return console.log(err)
+        console.log('Done!')
+        console.log(`File saved to: ${outputFilePath}`)
+
+        inputStream.close()
+        outputStream.close()
+      }
+    )
   })
 
-  result.push({
-    date: data.Date,
-    waitingTime
-  })
-})
-.on('end', function(){
-  // write the result to output file
-  outputStream.write(
-    JSON.stringify(result, null, 2),
-    err => {
-      if (err) return console.log(err)
-      console.log('Done!')
-      console.log(`File saved to: ${OUTPUT_FILE}`)
-    }
-  )
-})
+  // start processing
+  console.log(`Input file: ${inputFilePath}`)
+  inputStream.pipe(csvStream)
+}
 
-// start processing
-inputStream.pipe(csvStream)
+// convert all values of the object to number
+function process(o) {
+  const result = {}
+  const keys = Object.keys(o)
+  keys.forEach(key => {
+    result[key] = Number(o[key])
+  })
+  return result
+}
